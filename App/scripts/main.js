89572a19e918e7081c2fa27e68ea0be163f12cb4
SaLiGiAisBack <36777927+SaLiGiAisBack@users.noreply.github.com>
2019-11-03 17:26:14 +0400

    3-4 slides

--- App/scripts/main.js
+++ App/scripts/main.js
@@ -1,5 +1,8 @@
 var swiper = new Swiper('.container-main-sayHello', {
     spaceBetween: 30,
+    autoplay: {
+        delay: 10000,
+    },
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
