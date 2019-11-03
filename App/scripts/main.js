26e58c8ea328be7cc580948f5f90de7f91feb8d0
SaLiGiAisBack <36777927+SaLiGiAisBack@users.noreply.github.com>
2019-11-03 11:45:06 +0400

    second

--- App/scripts/main.js
+++ App/scripts/main.js
@@ -1,1 +1,10 @@
-
+var swiper = new Swiper('.container-main-sayHello', {
+    spaceBetween: 30,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '"></span>';
+        },
+    },
+});
