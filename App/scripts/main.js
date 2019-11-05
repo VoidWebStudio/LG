16a0b8394888f8186a0be62c53abd17d9ae1eb8f
SaLiGiAisBack <36777927+SaLiGiAisBack@users.noreply.github.com>
2019-11-05 05:00:22 +0400

    final

--- App/scripts/main.js
+++ App/scripts/main.js
@@ -8,6 +8,7 @@ var swiper = new Swiper('.container-main-sayHello', {
     navigation: {
         nextEl: '.ready',
     },
+    hashNavigation: true,
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
@@ -16,4 +17,43 @@ var swiper = new Swiper('.container-main-sayHello', {
         },
     },
 
+});
+var swiper2 = new Swiper('.container-slider', {
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: '.arrow-right',
+        prevEl: '.arrow-left',
+    },
+
+
+});
+
+var reload_button = document.getElementsByClassName("repeat");
+for (var i = 0; i < reload_button.length; i++) {
+    reload_button[i].onclick = function() {
+        history.go(0);
+}
+}
+
+$(".btn-1").click(function () {
+    $("#content-1").hide();
+    $("#yt")[0].src += "?autoplay=1";
+    $("#yt").show();
+});
+$(".btn-2").click(function () {
+    $("#content-2").hide();
+    $("#yt-2")[0].src += "?autoplay=1";
+    $("#yt-2").show();
+});
+$(".btn-3").click(function () {
+    $("#content-3").hide();
+    $("#yt-3")[0].src += "?autoplay=1";
+    $("#yt-3").show();
+});
+$(".btn-4").click(function () {
+    $("#content-4").hide();
+    $("#yt-4")[0].src += "?autoplay=1";
+    $("#yt-4").show();
 });
